26bd41f6e72aa346298f08b1f83668cc8dab737a
Saurav Kadavath <sauravkadavath@berkeley.edu>
2020-08-29 22:17:27 -0700

    Started 4 walker_stand experiments

--- gpu_run.py
+++ gpu_run.py
@@ -22,47 +22,47 @@ class Config:
 
     # Specifies tasks to run. It maps tmux session name to the command to run in that session.
     JOBS = {
-        "walker_walk_noss_noise2net_seed0" : "python3 src/train.py \
+        "walker_stand_noss_noise2net_seed0" : "python3 src/train.py \
                 --domain_name walker \
-                --task_name walk \
+                --task_name stand \
                 --action_repeat 4 \
                 --mode train \
                 --num_shared_layers 8 \
                 --seed 0 \
-                --work_dir logs/walker_walk/no_ss/noise2net_seed0 \
+                --work_dir logs/walker_stand/no_ss/noise2net_seed0 \
                 --save_model \
                 --neural_aug_type=noise2net",
         
-        "walker_walk_noss_noise2net_seed1" : "python3 src/train.py \
+        "walker_stand_noss_noise2net_seed1" : "python3 src/train.py \
                 --domain_name walker \
-                --task_name walk \
+                --task_name stand \
                 --action_repeat 4 \
                 --mode train \
                 --num_shared_layers 8 \
                 --seed 1 \
-                --work_dir logs/walker_walk/no_ss/noise2net_seed1 \
+                --work_dir logs/walker_stand/no_ss/noise2net_seed1 \
                 --save_model \
                 --neural_aug_type=noise2net",
         
-        "walker_walk_noss_noise2net_seed2" : "python3 src/train.py \
+        "walker_stand_noss_noise2net_seed2" : "python3 src/train.py \
                 --domain_name walker \
-                --task_name walk \
+                --task_name stand \
                 --action_repeat 4 \
                 --mode train \
                 --num_shared_layers 8 \
                 --seed 2 \
-                --work_dir logs/walker_walk/no_ss/noise2net_seed2 \
+                --work_dir logs/walker_stand/no_ss/noise2net_seed2 \
                 --save_model \
                 --neural_aug_type=noise2net",
         
-        "walker_walk_noss_noise2net_seed3" : "python3 src/train.py \
+        "walker_stand_noss_noise2net_seed3" : "python3 src/train.py \
                 --domain_name walker \
-                --task_name walk \
+                --task_name stand \
                 --action_repeat 4 \
                 --mode train \
                 --num_shared_layers 8 \
                 --seed 3 \
-                --work_dir logs/walker_walk/no_ss/noise2net_seed3 \
+                --work_dir logs/walker_stand/no_ss/noise2net_seed3 \
                 --save_model \
                 --neural_aug_type=noise2net",
     }

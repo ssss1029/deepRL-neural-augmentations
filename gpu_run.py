1d05832ab7e8c919f02bac97440b8860859cb659
Saurav Kadavath <sauravkadavath@berkeley.edu>
2020-08-31 15:54:02 -0700

    Finish some walker exps

--- gpu_run.py
+++ gpu_run.py
@@ -22,27 +22,58 @@ class Config:
 
     # Specifies tasks to run. It maps tmux session name to the command to run in that session.
     JOBS = {
-        "walker_walk_noss_noise2net_seed0_replayBuffer500k" : "python3 src/train.py \
-                --domain_name walker \
-                --task_name walk \
+        
+        #####################################################################################
+        #### SHADOWFAX
+        #####################################################################################
+
+        "cartpole_balance_noss_noise2net_seed0_replayBuffer500k" : "python3 src/train.py \
+                --domain_name cartpole \
+                --task_name balance \
                 --action_repeat 4 \
                 --mode train \
                 --num_shared_layers 8 \
                 --seed 0 \
-                --work_dir logs/walker_walk/no_ss/noise2net_seed0_replayBuffer500k \
+                --work_dir logs/cartpole_balance/no_ss/noise2net_seed0_replayBuffer500k \
                 --save_model \
                 --neural_aug_type=noise2net",
-        
-        # "walker_walk_noss_noise2net_seed1_replayBuffer500k" : "python3 src/train.py \
-        #         --domain_name walker \
-        #         --task_name walk \
-        #         --action_repeat 4 \
-        #         --mode train \
-        #         --num_shared_layers 8 \
-        #         --seed 1 \
-        #         --work_dir logs/walker_walk/no_ss/noise2net_seed1_replayBuffer500k \
-        #         --save_model \
-        #         --neural_aug_type=noise2net",
+
+        #####################################################################################
+        #### SMAUG
+        #####################################################################################
+
+        "finger_spin_noss_noise2net_seed0_replayBuffer500k" : "python3 src/train.py \
+                --domain_name finger \
+                --task_name spin \
+                --action_repeat 4 \
+                --mode train \
+                --num_shared_layers 8 \
+                --seed 0 \
+                --work_dir logs/finger_spin/no_ss/noise2net_seed0_replayBuffer500k \
+                --save_model \
+                --neural_aug_type=noise2net",
+
+        "finger_turn_easy_noss_noise2net_seed0_replayBuffer500k" : "python3 src/train.py \
+                --domain_name finger \
+                --task_name turn_easy \
+                --action_repeat 4 \
+                --mode train \
+                --num_shared_layers 8 \
+                --seed 0 \
+                --work_dir logs/finger_turn_easy/no_ss/noise2net_seed0_replayBuffer500k \
+                --save_model \
+                --neural_aug_type=noise2net",
+
+        "cheetah_run_noss_noise2net_seed0_replayBuffer500k" : "python3 src/train.py \
+                --domain_name cheetah \
+                --task_name run \
+                --action_repeat 4 \
+                --mode train \
+                --num_shared_layers 8 \
+                --seed 0 \
+                --work_dir logs/cheetah_run/no_ss/noise2net_seed0_replayBuffer500k \
+                --save_model \
+                --neural_aug_type=noise2net",
     }
 
     # Time to wait between putting jobs on GPUs (in seconds). This is useful because it might take time 
